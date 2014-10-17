fa5638b6796c77a8b2c817d31d90fea57a9e366a
Matthias Neid <matthias.neid@ser.de>
2014-10-17 14:13:42 +0200

    hgh

--- BomberCowServer/Tester.cs
+++ BomberCowServer/Tester.cs
@@ -12,7 +12,7 @@ namespace BomberCowServer
         static void Main(string[] args)
         {
             // set console title
-            Console.Title = "BomberCowServer v0.0.0.0.0.0.0.0.00.03";
+            Console.Title = "BomberCowServer v0.0.0.0.0.0.0.O.00.15";
 
             // new game
             Game game = new Game();
